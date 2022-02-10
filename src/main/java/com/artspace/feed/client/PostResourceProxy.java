771d28db3ca2acf198b9cc2639fb496cbb9144fc
arturfigueira <45975680+arturfigueira@users.noreply.github.com>
2022-02-09 21:08:37 -0300

    Temp Commit

--- src/main/java/com/artspace/feed/client/PostResourceProxy.java
+++ src/main/java/com/artspace/feed/client/PostResourceProxy.java
@@ -1,6 +1,7 @@
 package com.artspace.feed.client;
 
 import com.artspace.feed.Post;
+import io.quarkus.oidc.client.reactive.filter.OidcClientRequestReactiveFilter;
 import io.smallrye.mutiny.Uni;
 import java.util.List;
 import javax.ws.rs.GET;
@@ -9,10 +10,12 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @Path("/api/posts")
 @Produces(MediaType.APPLICATION_JSON)
+@RegisterProvider(OidcClientRequestReactiveFilter.class)
 @RegisterRestClient(configKey = "post-api")
 public interface PostResourceProxy {
 
